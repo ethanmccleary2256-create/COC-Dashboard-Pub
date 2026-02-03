7aa6f3e516868d7b37ab6812a05b3fc76b0b6fe0
ethanmccleary2256-create <ethanmccleary2256@gmail.com>
2026-02-03 13:19:06 -0600

    Vite config commit

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/coc-dashboard-pub/',
   plugins: [react()],
 })
