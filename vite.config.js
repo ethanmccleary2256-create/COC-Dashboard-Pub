a6605ced33315e848567f18a43bebc1f71ce9397
ethanmccleary2256-create <ethanmccleary2256@gmail.com>
2026-02-03 14:59:45 -0600

    vite config fix 1

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/coc-dashboard-pub/',
+  base: '/COC-Dashboard-Pub/',
   plugins: [react()],
 })
